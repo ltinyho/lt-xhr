e65b1c63a8f1549d9e891761cdb6012101348a1f
ltinyho <ltinyho@gmail.com>
2018-06-11 22:10:31 +0800

    add:dist

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@ export declare class Xhr {
     static cache: LruCache.Cache<{}, {}>;
     constructor();
     static xhr(setting: XhrRequestConfig): Promise<any>;
-    static custom(settting: CustomConfig): (set: any) => Promise<any>;
+    static custom(settting: CustomConfig): (set: XhrRequestConfig) => Promise<any>;
 }
 declare const xhr: typeof Xhr.xhr;
 export default xhr;
