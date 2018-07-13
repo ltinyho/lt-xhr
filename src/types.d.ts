ae81b7b85f7196ef8bade484ca085547b32f5849
ltinyho <ltinyho@gmail.com>
2018-07-13 16:57:37 +0800

    update:type

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,8 @@ export interface XhrRequestConfig extends AxiosRequestConfig {
   cache?: boolean | {
     filter?(AxiosResponse: AxiosResponse['data']): boolean
   }
+
+  [key: string]: any
 }
 
 export interface CustomConfig {
